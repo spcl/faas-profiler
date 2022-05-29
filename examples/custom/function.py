4722218285ad82f999cfc7889c8d176d191659be
Malte Wächter <git@malte-waechter.com>
2022-05-29 22:44:26 +0200

    Add simple custom example function

---
+++ examples/custom/function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Custom Lambda Function
+"""
+
+import faas_profiler as fp
+
+fp.profile()
+def handler(event, context):
+    a = [1] * (10 ** 6)
+    b = [2] * (2 * 10 ** 7)
+    del b
+    return a
+
+
+if __name__ == "__main__":
+    handler(None, None)
