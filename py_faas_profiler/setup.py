c2616e6d3d59f26eab6a4d19cf3b2ef761fb59f7
Malte Wächter <git@malte-waechter.com>
2022-06-16 20:31:09 +0200

    Add package setup routine

---
+++ py_faas_profiler/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+FaaS-Profiler Python Client setup routine.
+"""
+
+import setuptools
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.read().splitlines()
+
+setuptools.setup(
+    name='faas_profiler_python',
+    version='0.0.1',
+    url='https://github.com/spcl/faas-profiler',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=requirements
+)
