47676b782ca5a7c6cfcf8f21e5be15e987a226a4
Malte Wächter <git@malte-waechter.com>
2022-06-18 18:15:23 +0200

    Finish first version of measurements

--- py_faas_profiler/measurements/base.py
+++ py_faas_profiler/measurements/base.py
@@ -5,12 +5,15 @@ Base for Measurements.
 
 Defines abstract base class for all measurements and measuring points
 """
+from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, List, NamedTuple, Type
-from multiprocessing import Process, Pipe, connection
+import logging
 
-if TYPE_CHECKING:
-    from py_faas_profiler.profiler import ProfileContext
+from typing import Dict, List, Tuple, Type
+from multiprocessing import Process, connection
+from inflection import underscore
+
+from py_faas_profiler.config import ProfileContext, MeasuringState
 
 
 class MeasurementError(RuntimeError):
@@ -23,14 +26,25 @@ class Measurement:
 
     Cannot be initialised.
     """
-    _measurement_ = None
-    _measurements_ = {}
+    _measurements_: Dict[str, Measurement] = {}
+
+    name: str = ""
+    name_parts: tuple = tuple()
+    key: str = ""
+
+    #
+    # Measurement factory methods
+    #
 
     @classmethod
-    def register(cls, name):
+    def register(cls, name, module_delimiter: str = "::"):
         def decorator(subclass):
             cls._measurements_[name] = subclass
-            subclass._measurement_ = name
+            subclass.name = name
+            subclass.name_parts = tuple(underscore(part)
+                                        for part in name.split(module_delimiter))
+            subclass.key = "_".join(subclass.name_parts)
+
             return subclass
         return decorator
 
@@ -42,13 +56,31 @@ class Measurement:
             raise MeasurementError(
                 f"Unknown measurement name {name}. Available measurements: {list(cls._measurements_.keys())}")
 
-    def __init__(self) -> None:
-        self._profiler_context: Type[ProfileContext] = None
-
-    def setUp(self):
+    def setUp(
+            self,
+            profiler_context: Type[ProfileContext],
+            config: dict = {}) -> None:
+        """
+        TODO
+        """
         pass
 
-    def tearDown(self):
+    def start(self) -> None:
+        """
+        TODO
+        """
+        pass
+
+    def stop(self) -> None:
+        """
+        TODO
+        """
+        pass
+
+    def tearDown(self) -> None:
+        """
+        TODO
+        """
         pass
 
 
@@ -59,3 +91,101 @@ class ParallelMeasurement(Measurement):
 
     def measure(self):
         pass
+
+
+class MeasurementProcess(Process):
+    """
+    Process to run all measurements that are to be executed in parallel to the main process.
+    """
+
+    _logger = logging.getLogger("MeasurementProcess")
+
+    def __init__(
+        self,
+        measurements: List[Tuple[ParallelMeasurement], dict],
+        profile_context: Type[ProfileContext],
+        pipe_endpoint: Type[connection.Connection],
+        refresh_interval: float = 0.1
+    ) -> None:
+        self.profile_context = profile_context
+        self.pipe_endpoint = pipe_endpoint
+        self.refresh_interval = refresh_interval
+        self.measurements = self._init_and_setup_measurements(measurements)
+
+        super(MeasurementProcess, self).__init__()
+
+    def run(self):
+        """
+        Process routine.
+
+        Starts all measurements first and then generates new measurement points interval-based.
+        Stops when the main process tells it to do so.
+        Then stops all measurements and sends the results to the main process.
+        """
+        self._logger.info("Measurement process started.")
+
+        self._start_measurements()
+        self.pipe_endpoint.send(MeasuringState.STARTED)
+
+        self._logger.info("Measurement process started measuring.")
+
+        state = MeasuringState.STARTED
+        while state == MeasuringState.STARTED:
+            self._measure()
+
+            if self.pipe_endpoint.poll(self.refresh_interval):
+                state = self.pipe_endpoint.recv()
+
+        self._logger.info("Measurement process stopped measuring.")
+
+        self._stop_measurements()
+        self.pipe_endpoint.send(MeasuringState.STOPPED)
+
+    def _init_and_setup_measurements(self,
+                                     measurements: List[Tuple[ParallelMeasurement],
+                                                        dict]) -> List[Type[ParallelMeasurement]]:
+        measurement_instances = []
+
+        for meas_cls, meas_config in measurements:
+            try:
+                meas_instance = meas_cls()
+                meas_instance.setUp(self.profile_context, meas_config)
+            except RuntimeError as err:
+                self._logger.error(f"Setting up {meas_cls.name} failed: {err}")
+
+        return measurement_instances
+
+    def _start_measurements(self):
+        """
+        Starts all measurements.
+
+        Does not stop on errors while starting measurements, logs errors only.
+        """
+        for meas in self.measurements:
+            try:
+                meas.start()
+            except RuntimeError as err:
+                self._logger.error(f"Starting {meas.name} failed: {err}")
+
+    def _measure(self):
+        """
+        Triggers all measuring methods.
+        """
+        for meas in self.measurements:
+            try:
+                meas.measure()
+            except RuntimeError as err:
+                self._logger.error(
+                    f"Calling measure of {meas.name} failed: {err}")
+
+    def _stop_measurements(self):
+        """
+        Stops all measurements.
+
+        Does not stop on errors while stopping measurements, logs errors only.
+        """
+        for meas in self.measurements:
+            try:
+                meas.stop()
+            except RuntimeError as err:
+                self._logger.error(f"Stopping {meas.name} failed: {err}")
