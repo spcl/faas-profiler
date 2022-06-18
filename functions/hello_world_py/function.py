ecd59552f02964b236a0a0cb80da47c9f5aac08d
Malte Wächter <git@malte-waechter.com>
2022-06-18 18:14:04 +0200

    Update py hello_world

--- functions/hello_world_py/function.py
+++ functions/hello_world_py/function.py
@@ -4,10 +4,17 @@
 A AWS Lambda function.
 """
 
+from time import sleep
 import py_faas_profiler as fp
+
 
 @fp.profile()
 def handler(event, context):
+    sleep(3)
     return {
         "message": "hello_world"
     }
+
+
+if __name__ == "__main__":
+    handler(None, None)
