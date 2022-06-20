a1c48d2a421111d2ac2a68f61faaca1b044ea64d
Malte Wächter <git@malte-waechter.com>
2022-06-20 17:18:35 +0200

    Add config file for profiler

--- functions/hello_world_py/function.py
+++ functions/hello_world_py/function.py
@@ -4,13 +4,22 @@
 A AWS Lambda function.
 """
 
-from time import sleep
 import py_faas_profiler as fp
 
+import boto3
+from random import randint
+from os.path import join, abspath, dirname
 
-@fp.profile()
+config_file = join(abspath(dirname(__file__)), "fp_config.yml")
+
+
+@fp.profile(config_file=config_file)
 def handler(event, context):
-    sleep(3)
+    file_name = f'/tmp/coffee_{randint(0, 100)}.jpg'
+
+    s3 = boto3.client('s3')
+    s3.download_file('faas-profiler-resources', 'coffee.jpg', file_name)
+
     return {
         "message": "hello_world"
     }
