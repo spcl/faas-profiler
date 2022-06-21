4972e2fa2219c02b7ddadb7cae4cbfd527a4306c
Malte Wächter <git@malte-waechter.com>
2022-06-21 18:34:58 +0200

    Add class for global configuration

--- py_faas_profiler/config.py
+++ py_faas_profiler/config.py
@@ -3,14 +3,22 @@
 """
 FaaS-Profiler configuration
 """
+from __future__ import annotations
 
+import logging
+import pkg_resources
+import yaml
+
+from os import getpid, mkdir
 from os.path import dirname, join, abspath, exists
+from uuid import uuid4
 from json import load
 from enum import Enum
 from dataclasses import dataclass
 from typing import Any, List, Type
-from functools import reduce
-import uuid
+from functools import reduce, cached_property
+from collections import namedtuple
+
 
 ROOT_DIR = abspath(dirname(__file__))
 SHARED_DIR = join(dirname(ROOT_DIR), "shared")
@@ -20,15 +28,118 @@ SCHEMAS_DIR = join(SHARED_DIR, "schemas")
 TMP_RESULT_DIR = abspath("/tmp")
 
 
-@dataclass
+def get_faas_profiler_version():
+    try:
+        return pkg_resources.get_distribution("py_faas_profiler").version
+    except pkg_resources.DistributionNotFound:
+        return "-"
+
+
+class Config:
+
+    ConfigItem = namedtuple('ConfigItem', ['name', 'parameters'])
+
+    _logger = logging.getLogger("Config")
+    _logger.setLevel(logging.INFO)
+
+    MEASUREMENTS_KEY = "measurements"
+    CAPTURES_KEY = "captures"
+    EXPORTERS_KEY = "exporters"
+
+    @classmethod
+    def load_from_file(cls, config_file: str) -> Type[Config]:
+        if exists(config_file):
+            try:
+                with open(config_file, "r") as fp:
+                    try:
+                        config = yaml.safe_load(fp)
+                        if isinstance(config, dict):
+                            return Config(
+                                config=config,
+                                config_file=config_file)
+
+                        raise ValueError(
+                            f"Profiler configuration {config_file} must be a dict, but got {type(config)}")
+                    except yaml.YAMLError as err:
+                        cls._logger.error(
+                            "Could not parse profiler config file: {err}")
+            except IOError as err:
+                cls._logger.error("Could not open profiler config file: {err}")
+
+        return Config()
+
+    def __init__(
+        self,
+        config: dict = {},
+        config_file: str = None
+    ) -> None:
+        self._config = config
+        self._config_file = config_file
+
+    @cached_property
+    def measurements(self):
+        return self._parse_to_config_items(
+            self._config.get(self.MEASUREMENTS_KEY, []))
+
+    @cached_property
+    def exporters(self) -> List[Config.ConfigItem]:
+        return self._parse_to_config_items(
+            self._config.get(self.EXPORTERS_KEY, []))
+
+    @cached_property
+    def captures(self) -> List[Config.ConfigItem]:
+        return self._parse_to_config_items(
+            self._config.get(self.CAPTURES_KEY, []))
+
+    def _parse_to_config_items(
+        self,
+        config_list: list
+    ) -> List[Config.ConfigItem]:
+        if not config_list:
+            return []
+
+        items = []
+        for config in config_list:
+            name = config.get("name")
+            if name:
+                items.append(Config.ConfigItem(
+                    name, config.get("parameters", {})))
+
+        return items
+
+
 class ProfileContext:
     """
-    Data classes for the context of the current profile run.
+    TODO:
     """
-    profile_run_id: Type[uuid.UUID]
-    profile_run_tmp_dir: str
-    pid: int
-    measurement_process_pid: int = None
+
+    def __init__(self) -> None:
+        self._profile_run_id = uuid4()
+        self._pid: int = getpid()
+        self._measurement_process_pid: int = None
+        self._tmp_dir = join(TMP_RESULT_DIR,
+                             f"faas_profiler_{self.profile_run_id}_results")
+
+        mkdir(self._tmp_dir)
+
+    def set_measurement_process_pid(self, pid: int) -> None:
+        self._measurement_process_pid = pid
+
+    @property
+    def pid(self) -> int:
+        return self._pid
+
+    @property
+    def measurement_process_pid(self):
+        return self._measurement_process_pid
+
+    @property
+    def profile_run_id(self):
+        return self._profile_run_id
+
+    @property
+    def tmp_results_dir(self):
+        return self._tmp_dir
 
 
 @dataclass
@@ -53,7 +164,14 @@ def average_measuring_points(points: List[MeasuringPoint]) -> Any:
 
 class MeasuringState(Enum):
     STARTED = 1
-    STOPPED = 3
+    STOPPED = 2
+    ERROR = -1
+
+
+@dataclass
+class ProcessFeedback:
+    state: MeasuringState
+    data: Any = None
 
 
 def load_schema_by_measurement_name(
