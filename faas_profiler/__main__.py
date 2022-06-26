0b5d4a7c5bed8c4da86bc6d40f21ac55ea3d0232
Malte Wächter <git@malte-waechter.com>
2022-06-26 15:51:40 +0200

    Add server to package entry

--- faas_profiler/__main__.py
+++ faas_profiler/__main__.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-FaaS-profiler main module.
-
-Entrypoint for console.
+FaaS-Profiler Entrypoints
 """
 
 import sys
 import logging
+import argparse
+
 from fire import Fire
 
 from faas_profiler import CLI
 
-def main():
+from faas_profiler.server import fp_server
+
+def cli():
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     logging.info("Starting FaaS-Profiler CLI.")
     Fire(CLI)
 
+def server():
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    logging.info("Starting FaaS-Profiler Server.")
+    fp_server.run(host="0.0.0.0", port=80)
+
 if __name__ == "__main__":
-    main()
+    cli()
