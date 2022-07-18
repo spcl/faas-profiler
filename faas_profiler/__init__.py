c7db9997bcf2e37a8a259a49c92810168fcea0a3
Malte Wächter <maltewaechter1@gmail.com>
2022-07-18 18:35:35 +0200

    Include templating for each provider

--- faas_profiler/__init__.py
+++ faas_profiler/__init__.py
@@ -10,11 +10,19 @@ import yaml
 import faas_profiler.cli as cli
 
 from typing import List, Type
-from cached_property import cached_property
 from os.path import join, exists
+from glob import glob
 
-from faas_profiler.config import EXAMPLES_DIR
-from faas_profiler.templating import HandlerTemplate, GitIgnoreTemplate, ServerlessTemplate
+from faas_profiler.config import EXAMPLES_DIR, Runtime, Provider
+from faas_profiler.templating import (
+    HandlerTemplate,
+    GitIgnoreTemplate,
+    AWSServerlessTemplate,
+    GCPServerlessTemplate,
+    AzureServerlessTemplate,
+    RequirementsTemplate,
+    TemplatingError
+)
 
 
 class CommandError(RuntimeError):
@@ -26,28 +34,46 @@ class Commands:
     Welcome to the local FaaS Profiler SDK to test serverless functions with the profiler.
     """
 
-    def __init__(self) -> None:
-        self.examples = ExamplesManager()
-        self._config = {}
-
-
-class ExamplesManager:
-    """
-    Manage and generate example serverless applications and functions.
-    """
-
-    def generate(self, application: str, function: str = None):
+    def new_application(self, application: str, runtime: str):
         """
-        Generates a new function and application
+        Generates a new application.
         """
-        app = Application.get_or_generate(application)
-        if cli.confirm("Do you want to deploy the application?"):
-            app.deploy()
+        try:
+            runtime = Runtime[runtime.upper()]
+        except KeyError:
+            cli.error(f"No runtime found with name: {runtime}."
+                      f"Available are: {list(Runtime)}")
+        else:
+            try:
+                Application.generate(application, runtime)
+            except ValueError as err:
+                cli.error(err)
+                return
 
-        if function:
-            app.generate_function(function)
-            if cli.confirm("Do you want to deploy the function?"):
-                app.deploy(function)
+            cli.success(f"Application {application} created!")
+
+    def new_function(
+            self,
+            application: str,
+            function: str,
+            provider: str = None):
+        """
+        Generates a new function
+
+        If provider is None, the function will be created for all providers
+        """
+        try:
+            provider = Provider[provider.upper()] if provider else None
+        except KeyError:
+            cli.error(f"No provider found with name: {provider}."
+                      f"Available are: {list(Provider)}")
+        else:
+            try:
+                app = Application.find_by(application)
+            except ValueError as err:
+                cli.error(err)
+            else:
+                app.generate_function(function, provider)
 
     def deploy(self, application: str, function: str = None) -> None:
         app = Application.find_by(application)
@@ -86,7 +112,7 @@ class Application:
     Represents one example applications.
     """
 
-    ACCEPTED_SLS_CONFIG = ['sls.yml', 'serverless.yml']
+    ACCEPTED_SLS_CONFIG = 'sls_*.yml'
 
     @staticmethod
     def find_all_applications() -> List[str]:
@@ -95,31 +121,34 @@ class Application:
         Includes the path if a serverless config file is present.
         """
         subfolders = [f.path for f in os.scandir(EXAMPLES_DIR) if f.is_dir()]
+
         valid_applications = filter(
-            lambda p: any(exists(join(p, sls_file))
-                          for sls_file in Application.ACCEPTED_SLS_CONFIG),
+            lambda p: any(glob(join(p, Application.ACCEPTED_SLS_CONFIG))),
             subfolders)
 
         return list(valid_applications)
 
     @classmethod
-    def get_or_generate(cls, application_name: str) -> Type[Application]:
-        app = cls.find_by(application_name)
-        if app is None:
-            return cls.generate(application_name)
-
-        return app
-
-    @classmethod
     def find_by(cls, application_name: str) -> Type[Application]:
         path = join(EXAMPLES_DIR, application_name)
         if path not in Application.find_all_applications():
-            return
+            raise ValueError(
+                f"No application with name {application_name} found.")
 
         return cls(application_name, path)
 
     @classmethod
-    def generate(cls, application_name: str) -> Type[Application]:
+    def generate(
+        cls,
+        application_name: str,
+        runtime: Runtime,
+        aws_region: str = "eu-central-1",
+        gcp_region: str = "europe-west3-a",
+        azure_region: str = ""
+    ) -> Type[Application]:
+        """
+        Generates a new example application inside 'examples'
+        """
         cli.out(f"Generating new application {application_name}")
         path = join(EXAMPLES_DIR, application_name)
         if path in Application.find_all_applications():
@@ -132,12 +161,35 @@ class Application:
         gitignore_file = GitIgnoreTemplate.render(path)
         cli.out(f"Created: {gitignore_file}")
 
-        serverless_file = ServerlessTemplate.render(path, context={
-            "application_name": application_name
+        aws_sls_file = AWSServerlessTemplate.render(path, context={
+            "application_name": application_name,
+            "runtime": runtime.value,
+            "region": aws_region
         })
-        cli.out(f"Created: {serverless_file}")
+        cli.out(f"Created: {aws_sls_file}")
+
+        gcp_sls_file = GCPServerlessTemplate.render(path, context={
+            "application_name": application_name,
+            "runtime": runtime.value,
+            "region": gcp_region
+        })
+        cli.out(f"Created: {gcp_sls_file}")
+
+        azure_sls_file = AzureServerlessTemplate.render(path, context={
+            "application_name": application_name,
+            "runtime": runtime.value,
+            "region": azure_region
+        })
+        cli.out(f"Created: {azure_sls_file}")
+
+        cls._generate_python_specific_files(path)
 
         return cls(application_name, path)
+
+    @classmethod
+    def _generate_python_specific_files(cls, path: str):
+        gcp_sls_file = RequirementsTemplate.render(path)
+        cli.out(f"Created: {gcp_sls_file}")
 
     def __init__(
         self,
@@ -147,56 +199,58 @@ class Application:
         self.name = name
         self.path = path
 
-        self._sls_config = None
-
-    @cached_property
-    def sls_config_path(self) -> str:
+    def get_sls_config_path(self, provider: Provider) -> str:
         """
-        Returns the path to the serverless config file.
-
-        Raises Error, if file not found or more than one is found.
+        Returns the serverless config file for given provider
         """
-        sls_files = filter(
-            exists, [join(self.path, s) for s in self.ACCEPTED_SLS_CONFIG])
-        sls_files = list(sls_files)
+        path = join(self.path, f"sls_{provider.value}.yml")
+        if not exists(path):
+            return None
 
-        if len(sls_files) == 0:
-            raise RuntimeError(
-                f"Could not find a serverless config file in {self.path}"
-                "Please check the application.")
+        return path
 
-        if len(sls_files) > 1:
-            raise RuntimeError(
-                f"Found more than one serverless config file in {self.path}"
-                "Please provide only one.")
-
-        return sls_files[0]
-
-    @cached_property
-    def sls_config(self) -> dict:
+    def get_sls_config(self, provider: Provider) -> dict:
         """
-        Returns parsed the serverless config
+        Returns the serverless config for given provider
         """
-        with open(self.sls_config_path, "r") as fp:
+        path = self.get_sls_config_path(provider)
+        if path is None:
+            return {}
+
+        with open(path, "r") as fp:
             return yaml.safe_load(fp)
 
-    def flush_config(self):
+    def get_functions(self, provider: Provider) -> dict:
+        """
+        Returns the functions of the application by provider
+        """
+        provider_config = self.get_sls_config(provider)
+        return provider_config.get("functions", {})
+
+    def flush_config(self, provider: Provider, config: dict) -> None:
         """
         Writes cached serverless config back to file
         """
-        with open(self.sls_config_path, "w") as fp:
-            yaml.dump(self.sls_config, fp,
+        sls_path = self.get_sls_config_path(provider)
+        if sls_path is None:
+            return
+
+        with open(sls_path, "w") as fp:
+            yaml.dump(config, fp,
                       sort_keys=False,
                       default_flow_style=False)
 
-        del self.__dict__['sls_config']
+    def get_runtime(self, provider: Provider) -> Runtime:
+        """
+        Returns the runtime of the application given the provider
+        """
+        runtime = self.get_sls_config(
+            provider).get("provider", {}).get("runtime")
 
-    @property
-    def functions(self) -> dict:
-        """
-        Returns a dict of currently defined serverless functions
-        """
-        return self.sls_config.get("functions", {})
+        try:
+            return Runtime(runtime)
+        except ValueError:
+            return Runtime.UNKNOWN
 
     def deploy(self, function_name: str) -> None:
         """
@@ -230,36 +284,50 @@ class Application:
     def generate_function(
         self,
         function_name: str,
-        function_handler: str = "handler"
+        provider: Provider = None,
+        function_handler: str = "handler",
     ):
         """
         Generates a new function inside the application
         """
         cli.out(
             f"Generating function {function_name} for application {self.name}")
-        if function_name in self.functions:
-            raise ValueError(
-                f"Function named {function_name} already exists in {self.name}")
 
         cli.out("Creating handler file...")
-        handler_file = HandlerTemplate.render(
-            target_dir=self.path,
-            file_name=function_name,
-            context={"handler_name": function_handler})
+        try:
+            handler_file = HandlerTemplate.render(
+                target_dir=self.path,
+                file_name=function_name,
+                context={"handler_name": function_handler})
+        except TemplatingError as err:
+            cli.error(err)
+            return
+
         cli.out(f"Created: {handler_file}")
 
-        cli.out("Adding function to serverless config...")
-        self._add_function_to_serverless(function_name, function_handler)
-        cli.out("Serverless config updated.")
+        for p in Provider:
+            if provider is not None and p != provider:
+                continue
+
+            if function_name in self.get_functions(p):
+                cli.error(
+                    f"Function named {function_name} already exists in {self.name}")
+                continue
+
+            cli.out(f"Adding function to {p.value} serverless config...")
+            self._add_function_to_serverless(
+                p, function_name, function_handler)
+            cli.out("Serverless config updated.")
 
     def _add_function_to_serverless(
         self,
+        provider: Provider,
         function_name: str,
-        function_handler: str
+        function_handler: str,
     ) -> None:
-        functions = self.sls_config.setdefault("functions", {})
-        functions[function_name] = {
+        sls_config = self.get_sls_config(provider)
+        sls_config.setdefault("functions", {})[function_name] = {
             "handler": f"{function_name}.{function_handler}"
         }
 
-        self.flush_config()
+        self.flush_config(provider, sls_config)
