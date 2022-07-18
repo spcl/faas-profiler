2ee6fa4fd27b0b0fff95c072f5efbcc70ab9b557
Malte Wächter <maltewaechter1@gmail.com>
2022-07-18 18:36:14 +0200

    Add provider and runtime enum

--- faas_profiler/config.py
+++ faas_profiler/config.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from enum import Enum
 from os.path import abspath, dirname, join
 
 PACKAGE_ROOT = abspath(dirname(__file__))
@@ -8,3 +9,15 @@ TEMPLATES_DIR = join(PACKAGE_ROOT, "templates")
 
 PROJECT_ROOT = abspath(dirname(PACKAGE_ROOT))
 EXAMPLES_DIR = join(PROJECT_ROOT, "examples")
+
+
+class Runtime(Enum):
+    UNKNOWN = "unknown"
+    PYTHON = "python3.8"
+    NODE = "node16.4"
+
+
+class Provider(Enum):
+    AWS = "aws"
+    GCP = "gcp"
+    AZURE = "azure"
