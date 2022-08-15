32a2cd95de3fb60c34a51d12b77e1d5d96843222
Malte Wächter <git@malte-waechter.com>
2022-08-15 18:24:27 +0200

    Extend global config

--- faas_profiler/config.py
+++ faas_profiler/config.py
@@ -1,23 +1,73 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+FaaS-Profiler global configuration
+"""
 
-from enum import Enum
 from os.path import abspath, dirname, join
 
 PACKAGE_ROOT = abspath(dirname(__file__))
-TEMPLATES_DIR = join(PACKAGE_ROOT, "templates")
-
 PROJECT_ROOT = abspath(dirname(PACKAGE_ROOT))
-EXAMPLES_DIR = join(PROJECT_ROOT, "examples")
+
+import os
+from typing import Type
+
+from faas_profiler_core.constants import Provider
+
+from faas_profiler.storage import RecordStorage, S3RecordStorage
+
+class Config:
+    """
+    FaaS-Profiler Visualizer configuration
+    """
+
+    def __init__(self) -> None:
+        self._storage = S3RecordStorage(self.storage_bucket)
+
+    @property
+    def provider(self) -> Provider:
+        """
+        Returns the cloud provider where the Visualizer is deployed.
+        """
+        # TODO: make this dynamic
+        return Provider.AWS
+
+    @property
+    def region(self):
+        """
+        Returns the region where the Visualizer is deployed.
+        """
+        if self.provider == Provider.AWS:
+            return os.environ.get("AWS_REGION", "eu-central-1")
+
+    @property
+    def storage_bucket(self) -> str:
+        """
+        Returns the bucket name for the records storage
+        """
+        return os.environ.get("AWS_RECORDS_BUCKET", "faas-profiler-records")
+
+    @property
+    def storage(self) -> Type[RecordStorage]:
+        """
+        Returns a storage.
+        """
+        return self._storage
 
 
-class Runtime(Enum):
-    UNKNOWN = "unknown"
-    PYTHON = "python3.8"
-    NODE = "node16.4"
+    @property
+    def examples_dir(self) -> str:
+        """
+        Returns the directory to serverless examples.
+        """
+        return join(PROJECT_ROOT, "examples")
+
+    @property
+    def templates_dir(self) -> str:
+        """
+        Returns the directory to templates.
+        """
+        return join(PACKAGE_ROOT, "templates")
 
 
-class Provider(Enum):
-    AWS = "aws"
-    GCP = "gcp"
-    AZURE = "azure"
+config = Config()
