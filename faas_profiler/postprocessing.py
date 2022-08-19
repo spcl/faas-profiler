a7726da468347d0429ea24f49f966aaaf189d011
Malte Wächter <git@malte-waechter.com>
2022-08-19 16:19:42 +0200

    Update postprocessing to combine trace to profiles

--- faas_profiler/postprocessing.py
+++ faas_profiler/postprocessing.py
@@ -5,15 +5,16 @@ FaaS-Profiler Post processing
 
 import sys
 import logging
-from typing import Dict, Type
-from uuid import UUID
+from typing import Dict, Set, Type
+from uuid import UUID, uuid4
+from tqdm import tqdm
 
 from faas_profiler_core.models import TracingContext, TraceRecord
 
-from faas_profiler.storage import S3RecordStorage
-from faas_profiler.models import Trace
+from faas_profiler.config import config
+from faas_profiler.models import Trace, Profile
 
-logging.basicConfig(stream=sys.stdout)
+# logging.basicConfig(stream=sys.stdout)
 
 
 def make_identifier_string(identifier: dict) -> str:
@@ -32,95 +33,157 @@ def make_identifier_string(identifier: dict) -> str:
     return "##".join(identifier)
 
 
-class Cache:
+class TraceCache:
+    """
+    In-Memory trace cached used for record processing.
+    """
 
     def __init__(self) -> None:
-        self.traces = {}
-
-        self._traces_by_id = {}
-        self._trace_ids = set()
+        self._traces_by_id: Dict[UUID, Trace] = {}
+        self._unique_trace_ids: Set[UUID] = set()
 
     def get_trace(self, trace_id: UUID):
+        """
+        Get cached trace by ID (if available)
+        """
         return self._traces_by_id.get(trace_id)
 
     def get_all_traces(self) -> Type[Trace]:
+        """
+        Get all unique cached traces
+        """
         return [
-            self._traces_by_id[tid] for tid in self._trace_ids]
+            self._traces_by_id[tid] for tid in self._unique_trace_ids]
 
-    def store_trace(self, trace: Type[Trace], forward_trace_id: UUID = None):
-        trace_id = forward_trace_id
+    @property
+    def number_of_traces(self) -> int:
+        """
+        Returns the number of unique traces.
+        """
+        return len(self._unique_trace_ids)
+
+    def cache_trace(self, trace: Type[Trace], override_trace_id: UUID = None):
+        """
+        Caches new trace.
+
+        If override trace ID is not None, this ID will be used for the mapping.
+        """
+        trace_id = override_trace_id
         if trace_id is None:
             trace_id = trace.trace_id
 
-        self._trace_ids.add(trace_id)
+        self._unique_trace_ids.add(trace_id)
         self._traces_by_id[trace_id] = trace
 
-    def remove_trace(self, trace_id: UUID):
-        if trace_id in self._trace_ids:
-            self._trace_ids.remove(trace_id)
+    def flush_trace(self, trace_id: UUID):
+        """
+        Flushes trace out of unique trace IDs.
+        """
+        if trace_id in self._unique_trace_ids:
+            self._unique_trace_ids.remove(trace_id)
 
 
-class TraceBuilder:
+class RecordProcessor:
+
+    @classmethod
+    def execute(cls):
+        """
+        Strategy for processing new records.
+        """
+        return cls()._execute()
 
     def __init__(self) -> None:
         self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.setLevel(logging.INFO)
 
-        self.storage = S3RecordStorage("faas-profiler-records")
-        self.cache = Cache()
-
+        self.trace_cache = TraceCache()
         self.inbound_requests: Dict[UUID, TracingContext] = {}
         self.outbound_requests: Dict[UUID, TracingContext] = {}
 
-    def execute(self):
+        self.profiles_by_func_key: Dict[str, Profile] = {}
+
+    def _execute(self) -> None:
         """
-        Builds traces based on trace records
+        Builds new traces out of unprocessed records
         """
         self.logger.info("Start building traces.")
         self.logger.info(
-            f"Found {len(self.storage.unprocessed_record_keys)} unprocessed records \n")
+            f"Found {len(config.storage.unprocessed_record_keys)} unprocessed records \n")
 
-        for record in self.storage.unprocessed_records():
-            trace_ctx = record.tracing_context
-            func_ctx = record.function_context
-            in_ctx = record.inbound_context
-            out_ctxs = record.outbound_contexts
+        for record in tqdm(
+            config.storage.unprocessed_records(), total=len(
+                config.storage.unprocessed_record_keys)):
+            self._process_record(record)
 
-            if trace_ctx is None:
-                self.logger.error(
-                    "Cannot process record without tracing context")
-                continue
+        self.logger.info(
+            f"\n Constructed {self.trace_cache.number_of_traces} traces. Post-processing them.")
+        for trace in tqdm(self.trace_cache.get_all_traces()):
+            self._process_trace(trace)
 
+        self.logger.info(
+            f"\n Combined {self.trace_cache.number_of_traces} profile. Storing them.")
+        for profile in tqdm(self.profiles_by_func_key.values()):
+            config.storage.store_profile(profile)
+
+    def _process_record(self, record: Type[TraceRecord]) -> None:
+        """
+        Process on trace record.
+        """
+        trace_ctx = record.tracing_context
+        func_ctx = record.function_context
+        in_ctx = record.inbound_context
+        out_ctxs = record.outbound_contexts
+
+        if trace_ctx is None:
+            self.logger.error(
+                "Cannot process record without tracing context")
+            return
+
+        self.logger.info(
+            f"Processing Record ID {trace_ctx.record_id} - Context: {trace_ctx} - Function Context: {func_ctx}")
+        record_trace = self._create_or_return_trace(trace_ctx.trace_id)
+        record_trace.add_record(record)
+
+        self.trace_cache.cache_trace(record_trace)
+
+        if trace_ctx.parent_id is None and in_ctx.resolvable:
+            self._resolve_inbound_context(record_trace, record)
+        else:
             self.logger.info(
-                f"Processing Record ID {trace_ctx.record_id} - Context: {trace_ctx} - Function Context: {func_ctx}")
-            record_trace = self._create_or_return_trace(trace_ctx.trace_id)
-            record_trace.add_record(record)
+                "Skipping inbound resolving. Parent ID is set or inbound context is not resolvable")
 
-            self.cache.store_trace(record_trace)
+        if out_ctxs:
+            self.logger.info(
+                f"Resolving {len(out_ctxs)} Outbound contexts.")
+            self._resolve_outbound_context(record_trace, record)
+        else:
+            self.logger.info(
+                "Skipping outbound resolving. No outbound contexts defined.")
 
-            if trace_ctx.parent_id is None and in_ctx.resolvable:
-                self._resolve_inbound_context(record_trace, record)
-            else:
-                self.logger.info(
-                    "Skipping inbound resolving. Parent ID is set or inbound context is not resolvable")
+        config.storage.mark_record_as_resolved(record_id=trace_ctx.record_id)
 
-            if out_ctxs:
-                self.logger.info(
-                    f"Resolving {len(out_ctxs)} Outbound contexts.")
-                self._resolve_outbound_context(record_trace, record)
-            else:
-                self.logger.info(
-                    "Skipping outbound resolving. No outbound contexts defined.")
+    def _process_trace(self, trace: Type[Trace]):
+        """
+        Postprocesses a cached trace.
+        """
+        root_function = trace.root_function
+        profile = self.profiles_by_func_key.get(root_function.function_key)
+        if profile:
+            profile.traces.append(trace)
+        else:
+            self.profiles_by_func_key[root_function.function_key] = Profile(
+                profile_id=uuid4(), traces=[trace], function_context=root_function)
 
-        for trace in self.cache.get_all_traces():
-            self.storage.upload_trace(trace)
+    """
+    Private helper Methods
+    """
 
     def _create_or_return_trace(self, trace_id: UUID) -> Type[Trace]:
         """
         Creates or returns a trace
         """
         self.logger.info(f"Search for trace with ID {trace_id}")
-        trace = self.cache.get_trace(trace_id)
+        trace = self.trace_cache.get_trace(trace_id)
         if trace:
             self.logger.info(f"Found trace with ID {trace_id}")
             return trace
@@ -135,7 +198,10 @@ class TraceBuilder:
         record: Type[TraceRecord]
     ) -> None:
         """
+        Resolves the inbound context of record
 
+        Child trace is the trace of the record. The goal is to find a parent trace in the outbound request mapping that triggered this record.
+        If this is not found, the inbound context is saved so that it can be found in an outbound context that arrives later.
         """
         inbound_context = record.inbound_context
         tracing_context = record.tracing_context
@@ -145,16 +211,16 @@ class TraceBuilder:
             self.logger.info(
                 f"Found Outbound request for Inbound identifier {identifier}")
             parent_context = self.outbound_requests[identifier]
-            parent_trace = self.cache.get_trace(parent_context.trace_id)
+            parent_trace = self.trace_cache.get_trace(parent_context.trace_id)
             if parent_trace:
                 self.logger.info(
                     f"Found Parent trace {parent_trace.trace_id} for parent tracing context {parent_context.trace_id}")
 
                 parent_trace.merge_trace(child_trace)
                 record.tracing_context.parent_id = parent_context.record_id
-                self.cache.store_trace(
-                    parent_trace, forward_trace_id=child_trace.trace_id)
-                self.cache.remove_trace(child_trace.trace_id)
+                self.trace_cache.cache_trace(
+                    parent_trace, override_trace_id=child_trace.trace_id)
+                self.trace_cache.flush_trace(child_trace.trace_id)
             else:
                 self.logger.info(
                     f"Cannot find parent trace for parent trace ID {parent_context.trace_id}")
@@ -174,7 +240,11 @@ class TraceBuilder:
         record: Type[TraceRecord]
     ) -> None:
         """
+        Resolves all outbound contexts of a record.
 
+        Parent trace is the trace of the record. The goal is to find a child trace for all outbound contexts
+        that were triggered by the outbound request.
+        If this cannot be found, the context is saved so that a child trace arriving later can find it.
         """
         tracing_context = record.tracing_context
 
@@ -184,7 +254,8 @@ class TraceBuilder:
                 self.logger.info(
                     f"Found Inbound request for Outbound identifier {identifier}")
                 child_context = self.inbound_requests[identifier]
-                child_trace = self.cache.get_trace(child_context.trace_id)
+                child_trace = self.trace_cache.get_trace(
+                    child_context.trace_id)
                 if child_trace:
                     self.logger.info(
                         f"Found Child trace {child_context.trace_id} for child tracing context {child_context.trace_id}")
@@ -194,9 +265,9 @@ class TraceBuilder:
                         parent_id=tracing_context.record_id,
                         root_child_record_id=child_context.record_id)
 
-                    self.cache.store_trace(
-                        parent_trace, forward_trace_id=child_trace.trace_id)
-                    self.cache.remove_trace(child_trace.trace_id)
+                    self.trace_cache.cache_trace(
+                        parent_trace, override_trace_id=child_trace.trace_id)
+                    self.trace_cache.flush_trace(child_trace.trace_id)
                 else:
                     self.logger.info(
                         f"Cannot find child trace for child trace ID {child_context.trace_id}")
