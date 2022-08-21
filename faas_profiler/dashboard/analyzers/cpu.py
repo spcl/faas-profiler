d749e46a03a7b6fbb84a164fd9a3dd9d42d9ec41
Malte Wächter <git@malte-waechter.com>
2022-08-21 23:07:03 +0200

    Add CPU Usage analyzer

---
+++ faas_profiler/dashboard/analyzers/cpu.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+CPU Analyzers
+"""
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import dash_core_components as dcc
+
+from typing import Type
+from dash import html
+
+from faas_profiler_core.models import CPUUsage
+
+from faas_profiler.dashboard.analyzers import Analyzer
+from faas_profiler_core.models import RecordData
+
+
+class CPUUsageAnalyzer(Analyzer):
+
+    def __init__(self, record_data: Type[RecordData]):
+        self.record_data = record_data
+        self.record_name = record_data.name
+        self.results = CPUUsage.load(self.record_data.results)
+
+        super().__init__(record_data)
+
+    def name(self) -> str:
+        """
+        Returns the name for the line analyzer
+        """
+        return "CPU Usage"
+
+    def render(self):
+        """
+        Returns a line chart for all recorded memory usages.
+        """
+        interval = self.results.interval
+        measuring_points = self.results.measuring_points
+
+        if len(measuring_points) == 0:
+            return html.P("No cpu usages recorded.")
+
+        n = len(measuring_points)
+        measuring_points = np.array(measuring_points)
+        time_interval = np.arange(0, n * interval, interval) * 1e-3
+
+        data = pd.DataFrame({
+            "Time (ms)": time_interval, "Usage (%)": measuring_points
+        })
+
+        fig = px.line(
+            data,
+            x="Time (ms)",
+            y="Usage (%)",
+            title="Memory-Usage")
+
+        return html.Div([
+            dcc.Graph(figure=fig)
+        ])
