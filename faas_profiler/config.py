bbec0245cf44d597b4ca0b6ef303398ff55f8e10
Malte Wächter <git@malte-waechter.com>
2022-09-22 14:03:24 +0200

    Support different storages in config

--- faas_profiler/config.py
+++ faas_profiler/config.py
@@ -4,17 +4,15 @@
 FaaS-Profiler global configuration
 """
 
+from faas_profiler_core.storage import RecordStorage, S3RecordStorage, GCPRecordStorage
+from faas_profiler_core.constants import Provider
+from typing import Type
+import os
 from os.path import abspath, dirname, join
 
 PACKAGE_ROOT = abspath(dirname(__file__))
 PROJECT_ROOT = abspath(dirname(PACKAGE_ROOT))
 
-import os
-from typing import Type
-
-from faas_profiler_core.constants import Provider
-
-from faas_profiler.storage import RecordStorage, S3RecordStorage
 
 class Config:
     """
@@ -22,38 +20,79 @@ class Config:
     """
 
     def __init__(self) -> None:
-        self._storage = S3RecordStorage(self.storage_bucket)
+        self._storage_bucket = None
+        self._provider = None
+        self._storage: Type[RecordStorage] = None
+        self._region = None
+        self._project_id = None
+
+        os.makedirs(self.temporary_dir, exist_ok=True)
 
     @property
     def provider(self) -> Provider:
         """
         Returns the cloud provider where the Visualizer is deployed.
         """
-        # TODO: make this dynamic
-        return Provider.AWS
+        return self._provider
 
-    @property
-    def region(self):
-        """
-        Returns the region where the Visualizer is deployed.
-        """
-        if self.provider == Provider.AWS:
-            return os.environ.get("AWS_REGION", "eu-central-1")
+    @provider.setter
+    def provider(self, provider: str) -> None:
+        if provider == "aws":
+            self._provider = Provider.AWS
+        elif provider == "gcp":
+            self._provider = Provider.GCP
+        else:
+            raise RuntimeError(f"Invalid provider {provider}")
 
     @property
     def storage_bucket(self) -> str:
         """
         Returns the bucket name for the records storage
         """
-        return os.environ.get("AWS_RECORDS_BUCKET", "faas-profiler-records")
+        return self._storage_bucket
+
+    @property
+    def region(self) -> str:
+        return self._region
+
+    @region.setter
+    def region(self, region) -> None:
+        self._region = region
+
+    @storage_bucket.setter
+    def storage_bucket(self, bucket) -> None:
+        self._storage_bucket = bucket
+
+    @property
+    def project_id(self) -> str:
+        """
+        GCP project id
+        """
+        return self._project_id
+
+    @project_id.setter
+    def project_id(self, project_id) -> None:
+        self._project_id = project_id
 
     @property
     def storage(self) -> Type[RecordStorage]:
         """
         Returns a storage.
         """
-        return self._storage
+        if self._storage is not None:
+            return self._storage
 
+        if not self.provider or not self.storage_bucket:
+            raise RuntimeError(
+                "Please set first provider and record bucket name")
+
+        if self.provider == Provider.AWS:
+            self._storage = S3RecordStorage(self.storage_bucket, self.region)
+        elif self.provider == Provider.GCP:
+            self._storage = GCPRecordStorage(
+                self.project_id, self.region, self.storage_bucket)
+
+        return self._storage
 
     @property
     def examples_dir(self) -> str:
@@ -68,6 +107,13 @@ class Config:
         Returns the directory to templates.
         """
         return join(PACKAGE_ROOT, "templates")
+
+    @property
+    def temporary_dir(self) -> str:
+        """
+        Returns the directory for temporary files.
+        """
+        return join(PROJECT_ROOT, "profiler_tmp")
 
 
 config = Config()
