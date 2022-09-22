f335d6878f92549a3ea0794171e3e55246125ce2
Malte Wächter <git@malte-waechter.com>
2022-09-22 14:04:55 +0200

    Remove core functions

--- faas_profiler/core.py
+++ faas_profiler/core.py
@@ -1,5 +1,5 @@
 """
-
+Core functions
 """
 
 import logging
@@ -16,53 +16,6 @@ _logger = logging.getLogger(__name__)
 """
 Profile methods
 """
-
-
-class ProfileAccess:
-
-    def __init__(self, profile: Type[Profile]) -> None:
-        self.profile = profile
-        self.traces = load_all_profile_traces(profile)
-
-        self._group_data = None
-
-    def get_all_record_data(self, data_name: str):
-        """
-        Returns all record data for one name
-        """
-        if self._group_data is None:
-            self._group_data = self._group_data_by_name()
-
-        return self._group_data.get(data_name, [])
-
-    def _group_data_by_name(self):
-        """
-        Groups all trace data by record data key.
-        """
-        grouped_data = {}
-
-        for trace in self.traces:
-            for record in trace.records:
-                for record_data in record.data:
-                    grouped_records = grouped_data.setdefault(
-                        record_data.name, {})
-                    grouped_traces = grouped_records.setdefault(
-                        trace.trace_id, {})
-
-                    grouped_traces[str(record)] = record_data.results
-
-        return grouped_data
-
-
-def add_trace_to_profile(profile: Type[Profile], trace: Type[Trace]) -> None:
-    """
-    Adds a trace to profile
-    """
-    _trace_id = trace.trace_id
-    if _trace_id is None:
-        raise ValueError("Cannot add trace to profile without trace ID.")
-
-    profile.trace_ids.add(_trace_id)
 
 
 def load_all_profile_traces(profile: Type[Profile]) -> List[Type[Trace]]:
@@ -84,80 +37,6 @@ Trace methods
 """
 
 
-def clear_trace_records(trace: Type[Trace]):
-    """
-    Resets all records for trace.
-    """
-    trace.records = []
-
-
-def add_record_to_trace(trace: Type[Trace], record: Type[TraceRecord]):
-    """
-    Adds record to trace.
-
-    Makes sure that trace id of record is the same than the trace
-    """
-    record.tracing_context.trace_id = trace.trace_id
-    trace.records.append(record)
-
-    assert record.tracing_context.trace_id == trace.trace_id
-
-
-def merge_traces(
-    parent_trace: Type[Trace],
-    child_trace: Type[Trace],
-    parent_id: UUID = None,
-    root_child_record_id: UUID = None
-) -> None:
-    """
-    Merges child trace into parent.
-
-    Complexity: O(num_of_child_records)
-    """
-    for record in child_trace.records:
-        record.tracing_context.trace_id = parent_trace.trace_id
-        if parent_id is not None and root_child_record_id == record.tracing_context.record_id:
-            record.tracing_context.parent_id = parent_id
-
-        add_record_to_trace(parent_trace, record)
-
-    clear_trace_records(child_trace)
-
-
-def get_trace_root_record(trace: Type[Trace]) -> Type[TraceRecord]:
-    """
-    Returns record with no parent ID.
-    Returns None if no records exists.
-
-    If multiple exists, return the record with oldest invoked at.
-    """
-    if not trace.records or len(trace.records) == 0:
-        return None
-
-    root_records = filter(
-        lambda r: not r.tracing_context.parent_id,
-        trace.records)
-    root_records = sorted(
-        root_records,
-        key=lambda r: r.function_context.invoked_at,
-        reverse=False)
-
-    return root_records[0]
-
-
-def get_trace_root_function(trace: Type[Trace]) -> Type[FunctionContext]:
-    """
-    Returns the function context of the root record.
-
-    Return None if no root function exists or the function has no function
-    """
-    root_record = get_trace_root_record(trace)
-    if root_record and root_record.function_context:
-        return root_record.function_context
-
-    return None
-
-
 def get_record_by_id(
     trace: Type[Trace],
     record_id: UUID
@@ -172,3 +51,37 @@ def get_record_by_id(
         return next(filtered_records)
     except StopIteration:
         return None
+
+
+def group_traces_data_by_key(
+    traces: List[Type[Trace]]
+) -> dict:
+    """
+    Groups record data of multiple traces by key.
+    """
+    _grouped_data = {}
+    for trace in traces:
+        for k, data in group_record_data_by_key(trace).items():
+            _grouped_trace_data = _grouped_data.setdefault(k, {})
+            _grouped_trace_data.setdefault(trace.trace_id, []).extend(data)
+
+    return _grouped_data
+
+
+def group_record_data_by_key(trace: Type[Trace]) -> dict:
+    """
+    Groups all record data of a trace by key
+    """
+    _grouped_data = {}
+    if not trace.records or len(trace.records) == 0:
+        return _grouped_data
+
+    for record in trace.records:
+        if not record.data or len(record.data) == 0:
+            continue
+
+        for data_key, record_data in record.data.items():
+            curr_data = _grouped_data.setdefault(data_key, [])
+            curr_data.append(record_data)
+
+    return _grouped_data
