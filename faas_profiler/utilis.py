8fd270480f44c0fe9556e67b07eadee1e2156091
Malte Wächter <git@malte-waechter.com>
2022-09-22 14:05:31 +0200

    Add methods to parse seconds to ms and bytes to kb

--- faas_profiler/utilis.py
+++ faas_profiler/utilis.py
@@ -7,6 +7,18 @@ Common utitils module
 import logging
 
 
+def seconds_to_ms(sec: float) -> float:
+    """
+    Seconds to ms
+    """
+    return sec * 1e4
+
+def bytes_to_kb(bytes: float) -> float:
+    """
+    Bytes to KB
+    """
+    return bytes * 1e-3
+
 class Loggable:
     def __init__(self):
         self.logger = logging.getLogger(self.__class__.__name__)
